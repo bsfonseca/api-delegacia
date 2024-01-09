12f9e75c7071b5d1f04b2c711f13cf065ec21ca0
bsfonseca <bs.fonseca12@gmail.com>
2024-01-09 10:39:51 -0300

    Rota para criar um criminoso

---
+++ src/controllers/criminoso.controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import { Criminoso } from "../models/criminoso.model";
+import repository from "../database/prisma.repository";
+
+export class CriminosoController {
+    public async criarCriminoso(req: Request, res: Response) {
+        try {
+            const { nome, cidade, genero, nacionalidade } = req.body;
+
+            if (!nome || !cidade || !genero || !nacionalidade) {
+                return res.status(400).send({
+                    ok: false,
+                    message: "Preencha todos os campos",
+                });
+            }
+            const criminoso = new Criminoso(nome, cidade, genero, nacionalidade);
+
+            const result = await repository.criminoso.create({
+                data: criminoso,
+            });
+
+            return res.status(201).send({
+                ok: true,
+                message: "Registro efetuado com sucesso",
+                data: result,
+            });
+        } catch (error: any) {
+            return res.status(500).send({
+                ok: false,
+                message: error.toString(),
+            });
+        }
+    }
+}
