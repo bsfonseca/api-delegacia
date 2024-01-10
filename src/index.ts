e070dd27624d06acf3c0d7fab98a91e53a814989
bsfonseca <bs.fonseca12@gmail.com>
2024-01-10 13:43:21 -0300

    Rotas de crime e arma

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { CriminosoController } from "./controllers/criminoso.controller";
+import { CrimeController } from "./controllers/crime.controller";
+import { ArmaController } from "./controllers/arma.controller";
 
 const app = express();
 app.use(express.json());
@@ -8,12 +10,24 @@ app.use(express.json());
 
 const criminosoController = new CriminosoController();
 
+const crimeController = new CrimeController();
+
+const armaController = new ArmaController();
+
 //Rotas criminoso
 app.post("/criminoso", criminosoController.criarCriminoso);
 app.get("/criminoso", criminosoController.listarCriminoso);
 app.get("/criminoso/:id", criminosoController.obterCriminosoID);
 app.put("/criminoso/:id", criminosoController.atualizarCriminoso);
 app.delete("/criminoso/:id", criminosoController.deletarCriminoso);
+
+//Rotas crime
+app.post("/criminoso/:id/crime", crimeController.criarCrime);
+app.get("/criminoso/:id/crime", crimeController.listarCrime);
+
+//Rotas Arma
+app.post("/criminoso/:id/crime/:idCrime/arma", armaController.criarArma);
+app.get("/ciminoso/:id/crime/:idCrime/arma", armaController.listarArma);
 
 app.listen(3335, () => {
     console.log("Api rodando");
